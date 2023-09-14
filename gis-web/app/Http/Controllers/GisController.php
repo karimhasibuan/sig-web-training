ffbd12a257243ec8e1204a9bd172cc96509e146e
Karim Hasibuan <amfibicoding19@gmail.com>
2023-09-14 22:55:56 +0700

    feat: create fuzzycmeans manual method.

--- gis-web/app/Http/Controllers/GisController.php
+++ gis-web/app/Http/Controllers/GisController.php
@@ -25,4 +25,50 @@ class GisController extends Controller
 
         return view('beranda', compact('dataJsonSumut'));
     }
+
+    private function zscore($data)
+    {
+        $countColumn = count($data[0]);
+
+        for ($i = 0; $i < $countColumn; $i++) {
+            $sum = 0;
+            for ($j = 0; $j < count($data); $j++) {
+                $sum += $data[$j][$i];
+            }
+            $mean = $sum / count($data);
+
+            $sum = 0;
+            for ($j = 0; $j < count($data); $j++) {
+                $sum += pow($data[$j][$i] - $mean, 2);
+            }
+            $std = sqrt($sum / count($data));
+
+            for ($j = 0; $j < count($data); $j++) {
+                $data[$j][$i] = ($data[$j][$i] - $mean) / $std;
+            }
+        }
+        return $data;
+    }
+
+    public function fuzzycmeans()
+    {
+        $dataCovid = DB::table('data_covid')->get();
+
+        $dataSelection = [];
+        for ($i = 0; $i < count($dataCovid); $i++) {
+            $dataSelection[] =
+                [
+                    floatval($dataCovid[$i]->konfirmasi),
+                    floatval($dataCovid[$i]->sembuh),
+                    floatval($dataCovid[$i]->meninggal)
+                ];
+        }
+
+        // dd($dataSelection);
+
+        // Normalisasi data
+        $dataNormalisasi = $this->zscore($dataSelection);
+
+        dd($dataNormalisasi);
+    }
 }
